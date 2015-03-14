77dabc3690d58391227914c19326ffdc397c2518
JB <macburgerjunior@gmail.com>
2015-03-14 07:18:50 +0100

    hope I get travis running soon

--- rake.rb
+++
@@ -1,6 +0,0 @@
-task default: %w[test]
-
-task :test do
-  gem install cinch
-  ruby "bot.rb"
-end
--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,6 @@
 task default: %w[test]
 
 task :test do
+  gem install cinch
   ruby "bot.rb"
 end
