c5ebd6a831fc615e5d0571653237ed3cfae0717a
Jonathan <greenbigfrog@gmail.com>
2015-09-03 17:53:22 +0200

    update example.config.rb to the community style guide

--- example.config.rb
+++ example.config.rb
@@ -1,30 +1,35 @@
 require 'configatron'
-#This is the config file! Please copy this file to config.rb before editing!
+# This is the config file! Please copy this file to config.rb before editing!
 
-#the Bot nick
+# The Bot nick
 configatron.nick = 'bugb0t'
-#the Server to connect to
+# The Server to connect to
 configatron.server = 'irc.freenode.net'
-#the channels to join
-configatron.channels = ["#botwar", "#dogecoin-bots", "#dogecoindark", "##dogechat", "#bugb0t"]
-#the realname of the bot
-configatron.realname = "Github: https://github.com/green-big-frog/bugb0t"
-#the user f.e. ~ILOVEFROG@[host]
-configatron.user = "ILOVEFROG"
-#delay joins so that there isn't always a changing host reconnect from the IRC server
+# The channels to join
+configatron.channels = [
+  '#botwar',
+  '#dogecoin-bots',
+  '#dogecoindark',
+  '##dogechat',
+  '#bugb0t']
+# The realname of the bot
+configatron.realname = 'Github: https://github.com/green-big-frog/bugb0t'
+# The user f.e. ~ILOVEFROG@[host]
+configatron.user = 'ILOVEFROG'
+# Delay joins so that there isn't always a "changing host" reconnect
 configatron.delay_joins = :identified
-#set how ofter the bot should max write
+# Set how ofter the bot should max write
 configatron.messages_per_second = 0.2
 
-#the nick of the admin
-configatron.admin.nick = "green-big-frog"
+# The nick of the admin
+configatron.admin.nick = 'green-big-frog'
 
-#options for lmgtfy
-configatron.options.lmgtfy.username = ""
-configatron.options.lmgtfy.api_key = ""
+# Options for lmgtfy
+configatron.options.lmgtfy.username = ''
+configatron.options.lmgtfy.api_key = ''
 
-#options for identify
-configatron.options.identify.username = "testb0t"
-configatron.options.identify.password = ""
-#which type of identifying f.e. Nickserv, Quakeserv etc
+# Options for identify
+configatron.options.identify.username = 'testb0t'
+configatron.options.identify.password = ''
+# Which type of identifying f.e. Nickserv, Quakeserv etc
 configatron.options.identify.type = :nickserv
