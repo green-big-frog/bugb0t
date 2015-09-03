0d17d24ca718c5131858b462ca2d4ed57fd8b4df
Jonathan <greenbigfrog@gmail.com>
2015-09-03 17:07:23 +0200

    Updating the codestyle to the ruby-style-guide

--- bot.rb
+++ bot.rb
@@ -1,56 +1,56 @@
-#bundle requirments
+# bundle requirments
 require 'rubygems'
 require 'bundler/setup'
 
-#configatron
+# configatron
 require 'configatron'
 require './config.rb'
 
 require 'optparse'
 require 'cinch'
 require 'cinch_hangman'
-require "cinch/plugins/identify"
+require 'cinch/plugins/identify'
 require 'cinch/plugins/lmgtfy'
 
+# Dir["./plugins/*.rb"].each {|file| require file }
 Dir["./plugins/*.rb"].each {|file| require file }
 
-
-def startTestBot
-	testBot = Cinch::Bot.new do
-		configure do |c|
-			c.nick = configatron.nick
-			c.server = configatron.server
-			c.plugins.plugins = [
-				Cinch::Plugins::PluginManagement,
-				Cinch::Plugins::LMGTFY,
-				Cinch::Plugins::Identify,
-				Cinch::Plugins::Joker,
-				Cinch::Plugins::Hangman,
-				Cinch::Plugins::Help,
-				Cinch::Plugins::Quotes,
-				Cinch::Plugins::Catfact,
-				Cinch::Plugins::Administration,
-				Cinch::Plugins::Pirate
-				]
-		end
-		on :connect do |m|
-			puts'aborting'
-			exit
-		end
-	end
-	puts'starting TestBot'
-	testBot.start
-	puts'started bot'
+def start_test_bot
+  test_bot = Cinch::Bot.new do
+    configure do |c|
+      c.nick = configatron.nick
+      c.server = configatron.server
+      c.plugins.plugins = [
+        Cinch::Plugins::PluginManagement,
+        Cinch::Plugins::LMGTFY,
+        Cinch::Plugins::Identify,
+        Cinch::Plugins::Joker,
+        Cinch::Plugins::Hangman,
+        Cinch::Plugins::Help,
+        Cinch::Plugins::Quotes,
+        Cinch::Plugins::Catfact,
+        Cinch::Plugins::Administration,
+        Cinch::Plugins::Pirate
+      ]
+    end
+    on :connect do |m|
+      puts 'aborting'
+      exit
+    end
+  end
+  puts 'starting TestBot'
+  test_bot.start
+  puts 'started bot'
 end
 
 options = {}
 OptionParser.new do |opts|
-	opts.banner = "Usage: ruby bot.rb [options]"
+  opts.banner = 'Usage: ruby bot.rb [options]'
 
-	opts.on("-t", "--test", "Run testmode") do |test|
-		puts'running test mode'
-		startTestBot
-	end
+  opts.on('-t', '--test', 'Run testmode') do |test|
+    puts 'running test mode'
+    start_test_bot
+  end
 end.parse!
 
 p options
@@ -77,27 +77,27 @@ bot = Cinch::Bot.new do
     c.delay_joins = configatron.delay_joins
     c.messages_per_second = configatron.messages_per_second
     c.plugins.plugins = [
-    	Cinch::Plugins::PluginManagement,
-    	Cinch::Plugins::LMGTFY,
-    	Cinch::Plugins::Identify,
-    	Cinch::Plugins::Joker,
-    	Cinch::Plugins::Hangman,
-    	Cinch::Plugins::Help,
-    	Cinch::Plugins::Quotes,
-    	Cinch::Plugins::Catfact,
-    	Cinch::Plugins::Administration,
-    	Cinch::Plugins::Pirate
-    	]
+      Cinch::Plugins::PluginManagement,
+      Cinch::Plugins::LMGTFY,
+      Cinch::Plugins::Identify,
+      Cinch::Plugins::Joker,
+      Cinch::Plugins::Hangman,
+      Cinch::Plugins::Help,
+      Cinch::Plugins::Quotes,
+      Cinch::Plugins::Catfact,
+      Cinch::Plugins::Administration,
+      Cinch::Plugins::Pirate
+    ]
     c.plugins.options = {
       Cinch::Plugins::LMGTFY => {
         "username" => configatron.options.lmgtfy.username,
         "api_key" => configatron.options.lmgtfy.api_key
       },
-      
+
       Cinch::Plugins::Identify => {
-      	:username => configatron.options.identify.username,
-      	:password => configatron.options.identify.password,
-      	:type     => configatron.options.identify.type
+        :username => configatron.options.identify.username,
+        :password => configatron.options.identify.password,
+        :type     => configatron.options.identify.type
       }
     }
   end
