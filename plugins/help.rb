27ba8d78f7aa78e1151698abb446b7c027348c0a
Jonathan <greenbigfrog@gmail.com>
2015-09-14 21:18:07 +0200

    fix identation for the help plugin

--- plugins/help.rb
+++ plugins/help.rb
@@ -7,7 +7,7 @@ module Cinch
 
       match "help"
       def execute(m)
-    	m.reply "#{m.user.nick}, you requested help!\tTo hear a joke do: !joke\tTo google for someone do !google [nick] [question]\tTo play hangman do !hang guess [letters]\tTo hear a Catfact: !cat\t To get a quote: !quote\tTo learn how to speak like a pirate: !pirate [Text]"
+        m.reply "#{m.user.nick}, you requested help!\tTo hear a joke do: !joke\tTo google for someone do !google [nick] [question]\tTo play hangman do !hang guess [letters]\tTo hear a Catfact: !cat\t To get a quote: !quote\tTo learn how to speak like a pirate: !pirate [Text]"
       end
     end
   end
